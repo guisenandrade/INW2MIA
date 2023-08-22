6c8db30a6fd47cc086b3565de538be60e7429549
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-22 13:43:23 -0300

    NODE

---
+++ node/hellonode.js
@@ -0,0 +1,13 @@
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+   res.statusCode = 200;
+   res.setHeader('Content-Type','text/plain')
+   res.end('Hello world, em node.js')
+}
+);
+
+const port = 3000;
+server.listen(port,()=>{
+    console.log(`Servidor rodando na url http://localhost:${port}`);
+});
