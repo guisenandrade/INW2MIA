c8400c5d570be78257feb1f76b040f9bbe0240ac
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-05 12:42:42 -0300

    mongo

--- mongo/index.js
+++ mongo/index.js
@@ -1,13 +1,16 @@
 //mongodb://localhost:27017
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/test",{useNewUrlParser : true, useUnifiedTopology:true})
+mongoose.connect('mongodb://localhost:27017/exemplo',
+{useNewUrlParser : true, 
+useUnifiedTopology:true,
+serverSelectionTimeoutMS : 5000});
 
 const db = mongoose.connection
 
-db.on("error", console.error.bind(console,"connection error : "))
+db.on('error', console.error.bind(console,'connection error : '))
 
 db.once("open" , function(){
-    console.log("Estamos conectados ao banco")
+    console.log('Estamos conectados ao banco')
 })
---
+++ mongo/readme.md
@@ -0,0 +1,3 @@
+Após clonar este projeto execute o comando abaixo:
+
+npm install
