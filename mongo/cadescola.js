e3281ff12f7c99110d8da72cf705b3e4df7ea58a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-11 10:07:43 -0300

    banco

---
+++ mongo/cadescola.js
@@ -0,0 +1,41 @@
+//configurar a conexão com o mongodb e o banco de dados
+
+const mongoose = require ('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/escola',
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        serverSelectionTimeoutMS: 30000
+    
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error : '))
+
+
+db.once('open', function(){
+    console.log("Estamos conectados ao mongoDB")
+})
+
+
+//criando uma collection dentro do mongodb
+
+const alunoSchema = new mongoose.Schema({
+    matricula : String,
+    nome : String,
+    idade : Number,
+    turma : String
+})
+
+const Aluno = mongoose.model("Aluno",alunoSchema)
+
+const paulo = new Aluno({
+    matricula : 'rm201',
+    nome: 'Paulo da Silva',
+    idade : 18,
+    turma:'2MIA'
+})
+
+paulo.save()
